df86444e31e9bf2ca677a3c6bde8960a3115782c
Cruzer-S <yyyynoom@gmail.com>
2024-06-22 07:21:59 -0400

    add logger.h

---
+++ include/logger.h
@@ -0,0 +1,41 @@
+#ifndef LOGGER_H__
+#define LOGGER_H__
+
+#include <stdio.h>
+#include <stdbool.h>
+
+#define LOGGER_MAX_LEVEL	10
+
+#define STRINGIFY_INTERNAL(X) # X
+#define STRINGIFY(X) STRINGIFY_INTERNAL(X)
+
+#define log(LOG, LEVEL, ...) 						\
+		logger_log(LOG, LOGGER_LEVEL_##LEVEL,		\
+			__FILE__, __func__, STRINGIFY(__LINE__),	\
+			__VA_ARGS__					\
+		)
+
+typedef struct logger *Logger;
+typedef struct log_level *LogLevel; 
+
+Logger logger_create(void);
+
+bool logger_define_level(Logger, int level, char *name, FILE * );
+bool logger_set_format(Logger , int level, char *format);
+
+int logger_log(Logger , int ,
+	const char *file, const char *function, const char *line,
+	const char *formatted, ...
+);
+
+// f : function name
+// n : file name
+// l : line number
+// s : formatted string
+// d : date
+// t : time
+// p : level name
+
+void logger_destroy(Logger );
+
+#endif
